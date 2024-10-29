530db28d9a56b9052ab7f488de14b492c746225b
Priyanka Kumari <priyankacoder11@gmail.com>
2024-10-29 14:57:04 +0530

    Create repositoryContent.js

---
+++ repositoryContent.js
@@ -0,0 +1,11 @@
+
+//javascriptscript   and javascriptscript1 (js)  : todolistnew and all code related to js   ->add all interview question js  related code  in this repository
+//interviewquestion  (react)  : Carousal ,CounterContext,UseContext ,,UseReducer , Calculator  ,dropdown , infinitives ,srcepicSongList, infinitives baller ,pagination ,usememo
+//Calculator1,InfinitiBaller, stoppauserese, todolist.jsx ,dropdown.jsx ,whethercard  ->add all interview question react related code  in this repository
+
+//company    : css -> position ,overlap , zindex ,carousal , (justairports) ->activeclass , focus,progress,responsive ->add all html and css in this repository
+//breadcrumb  : reducer , redux , routing   in react
+//javascriptCode : productpage-main for api call , todo , whether-app   in react 
+// usefulMaterial (react) : breadcrumb , carousal ,reducer ,closure , hooks,pagination ,redux ,routing ,store ,todolist ,todoUsingRedux
+
+
