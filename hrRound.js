ed7b939def4a278deaa9b41ce9c947522e246f8f
Priyanka Kumari <priyankacoder11@gmail.com>
2024-10-29 14:55:16 +0530

    Create hrRound.js

---
+++ hrRound.js
@@ -0,0 +1,27 @@
+
+// reason => I’m seeking new challenges to further develop my skills and work on diverse projects beyond 
+// my current role, where I've primarily managed marketing websites. I’m eager to grow in a dynamic  environment.
+
+
+//Intoduction => I'm a React developer focused on building robust,user friendly  and  scalable, responsive  website With a strong foundation in JavaScript, React, and modern frameworks 
+// like Redux and Tailwind CSS. i have developed streamlined ,accessible site for mmwmachine , created a job aggregator to simplify career searches
+//built an e-commerce platform for online pharmaceuticals focused on responsive 
+// and a seamless user experience. I'm committed to building scalable solutions that prioritize usability and performance."
+
+
+//1careerwallet in eglogics   ,indimedo ,mmw,Mohindra Engineering Company  -> live link ,4  fetaures  , website view ,tech 
+// Created a user-friendly job aggregator, simplifying career searches. enhance efficiency and accessibility with the help of optimize code , facilitating smoother transitions into desired professions.
+//Developed a robust platform for online pharmaceuticals, Streamlined user experience and ensured secure transactions, contributing to accessible healthcare solutions.
+//A user-friendly and mobile-responsive platform designed for seamless navigation and fast loading times. Built with a robust tech stack, including CSS, HTML, Bootstrap, and Javascript.
+//Built with a robust tech stack including CSS, HTML, Bootstrap, and Php, we ensure a seamless user experience for our visitors. I thrive in collaborative environments, continually seeking opportunities.
+
+
+// -> introduction and project  and reason  samall company -> design landing page  - github priyanka807,never458032 , you know in which folder where you will get extact code and sec oftion for googling 
+// -> large company ->functionality ,react ,core concept of react ,task assign based on the functionality
+//-> visual code for normal html and react open 2 github 3 portfolio 4 cheeting 5 clear notes where mentioned where will you which code you will get
+
+//common high-figh words 1 robust 2 user friendly 3 streamlined 4streamless 5 accessablity 6 responsive 7 accessability 
+//robust and scableable,streamlined and accessible ,seamless user experience ,scalable solution 
+
+
+//1 streamlined and accessible  2 seamless user experience  3 accessability and useability , thrive in collabrative environments ,continuousaly seeking opportunities .
